7ccbcc6fb1e54cde01341c766abdc063898b2b4e
Nguyen Quoc Hung <nguyenquochung.workvn@gmail.com>
2025-08-30 23:32:14 +0700

    feat: trigger cache of ios

--- public/sw.js
+++ public/sw.js
@@ -6,8 +6,8 @@ importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
 
 // Cache name for app shell and dynamic content
-const CACHE_NAME = 'fund-flow-cache-v1-30082025';
-const DYNAMIC_CACHE = 'fund-flow-dynamic-cache-v1-30082025';
+const CACHE_NAME = 'fund-flow-cache-v1-30082025-02';
+const DYNAMIC_CACHE = 'fund-flow-dynamic-cache-v1-30082025-02';
 
 // Resources to cache immediately (app shell)
 const APP_SHELL = [
